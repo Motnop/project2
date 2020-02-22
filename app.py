b94a204668caa9593e97866565daf6799345e0da
Pontom <motnop@gmail.com>
2020-02-22 14:33:25 +0300

    version 2

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 import data
 
 
-def count_derection(direction):
+def count_derection_stats(direction):
     count = 0
     price = []
     night = set()
@@ -15,29 +15,45 @@ def count_derection(direction):
     price.sort()
     min = price[0]
     max = price[-1]
-    return {'count': count, 'min': min, 'max': max, 'night_min': sorted(night)[0], 'night_max': sorted(night)[-1]}
+    return {
+            'count': count,
+            'min': min,
+            'max': max,
+            'night_min': sorted(night)[0],
+            'night_max': sorted(night)[-1]
+            }
 
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', departure=data.departures, title=data.title, subtitle=data.subtitle,
-                           description=data.description, tours=data.tours)
+    return render_template('index.html',
+                            departures=data.departures, title=data.title,
+                            subtitle=data.subtitle,
+                            description=data.description, tours=data.tours
+                            )
 
 
 @app.route('/departure/<direction>')
 def departure(direction):
     if direction not in data.departures:
-        return "Таких направлений нет"
+        return "Таких направлений не найдено!"
     else:
-        return render_template('departure.html', dep=count_derection(direction),
-                               direction=data.departures[direction], tours=data.tours, churl=direction)
+        return render_template('departure.html',
+                                departures=data.departures, title=data.title,
+                                stats=count_derection_stats(direction),
+                                direction=data.departures[direction],
+                                tours=data.tours,
+                                churl=direction
+                                )
 
 
 @app.route('/tour/<id>')
 def tour(id):
-
-    return render_template('tour.html', tours=data.tours[int(id)], departure=data.departures)
+    return render_template('tour.html',
+                            departures=data.departures, title=data.title,
+                            tour=data.tours[int(id)],
+                            )
 
 app.run('0.0.0.0', port=8000, debug=True)
