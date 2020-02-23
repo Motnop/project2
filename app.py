bf236525f3cf5c1dea801ab8b74d08d3f3d821a7
Pontom <motnop@gmail.com>
2020-02-23 23:40:07 +0300

    version 4 (with-aling, new func) + gunicorn

--- app.py
+++ app.py
@@ -60,4 +60,6 @@ def tour(id):
                             tour=data.tours[int(id)],
                             )
 
-app.run('0.0.0.0', port=8000, debug=True)
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=8000, debug=True))
